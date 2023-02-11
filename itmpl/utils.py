7f6dae5b095800c7b2d9cb1c765923004146f61e
Isaac Harris-Holt <isaac@harris-holt.com>
2023-02-11 14:26:51 +0000

    Appease pyright

--- itmpl/utils.py
+++ itmpl/utils.py
@@ -6,7 +6,15 @@ from types import ModuleType
 def import_external_module(module_path: Path) -> ModuleType:
     """Import a module from an external path."""
     spec = importlib.util.spec_from_file_location("itmpl", module_path)
+
+    if spec is None:
+        raise ValueError(f"No module found at path: {module_path}")
+
     module = importlib.util.module_from_spec(spec)
+
+    if spec.loader is None:
+        raise ValueError(f"No loader found for module: {module_path}")
+
     spec.loader.exec_module(module)
 
     return module
